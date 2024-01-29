68120c9b471cae4b81e5ead4f85a32189e32ea14
BhagyashreeJv <158039403+BhagyashreeJv@users.noreply.github.com>
2024-01-29 15:37:53 +0000

    container config done

---
+++ 27thJan_JS/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs,not browser hence this will not work as it works in browser
+
+console.log(3
+    +
+    3); //code credibility should be high
+
+console.log("alisha");
+
+let sname="alisha"
+let age=20
+let isLoggedIn=false
+let State;
+
+console.log(typeof state);
